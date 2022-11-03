80895afff45c533389c199479797eb1ccde41fc1
Prashant Bansal <109963029+prashantb088@users.noreply.github.com>
2022-11-03 19:40:03 +0530

    Add files via upload

---
+++ usermgm/src/main/java/com/usermgm/UsermgmApplication.java
@@ -0,0 +1,18 @@
+package com.usermgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@SpringBootApplication
+public class UsermgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermgmApplication.class, args);
+	}
+
+}
